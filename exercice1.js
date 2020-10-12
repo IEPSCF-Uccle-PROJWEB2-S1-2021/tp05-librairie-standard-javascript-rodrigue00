a2b500e5845cd68ef4545ba054e700982e87ac45
rodrigue <ydjamen@yahoo.fr>
2020-10-12 22:34:12 +0200

    fin

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,37 @@
+let msg = "";
 function chuckNorris(msg) {
-  return "";
+  let result = "";
+  let binaire = binary(msg);
+  console.log(binaire);
+
+  let premier = "";
+  for (let index = 0; index < binaire.length; index++) {
+
+    if (index !== 0)
+      result += " ";
+    if (premier !== binaire.charAt(index)) {
+      if (binaire.charAt(index) === "0")
+        result += "00 0";
+      else
+        resul += "0 0";
+    }
+    else {
+      result = result + "0";
+    }
+    premier = binaire.charAt(index);
+  }
+  return result;
 }
+
+
+
+function binary(numba) {
+  let resul = "";
+  for (let index = 0; index < numba.length; index++) {
+    let binar = numba.charCodeAt(index).toString(2);
+    resul += binar;
+
+  }
+  return resul;
+}
+
--- exercice2.js
+++ exercice2.js
@@ -1,3 +1,18 @@
+let Temperature =5550,data ="";
 function analyzeTemperature(quantity, data) {
-  return 0;
+  if(quantity <=0)
+    return 0;
+  else if(quantity <=1000)
+  {
+    let dat=data.split(' ');
+
+    for(let i=0;i<dat.length ; i++)
+     { let element= parseInt(dat[i])
+        if(Math.abs(element) <= Temperature)
+        Temperature=element;
+      }
+    
+  }
+  return Temperature;
 }
+console.log(analyzeTemperature(5," -2 -8 4 5") );
